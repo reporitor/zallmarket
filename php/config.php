df1b2ab99a481da3d897e59b926f3be71082a137
reporitor <ajapong576@gmail.com>
2025-06-08 04:28:24 -1100

    Create config.php

---
+++ php/config.php
@@ -0,0 +1,11 @@
+<?php
+// Konfigurasi Database
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'gamehub_db');
+
+// Konfigurasi Website
+define('SITE_URL', 'http://localhost/gamehub');
+define('ADMIN_EMAIL', 'admin@gamehub.com');
+?>
